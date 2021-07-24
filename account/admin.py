d4fefe02927f90e3494ff3547862366446198842
Chakpuia <vlc2112d@gmail.com>
2021-07-24 18:22:51 +0530

    no message

--- account/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-models_registered = [Location,LocationChoices,Profile,IkigaiCategory,Ikigais,UserIkigai,SkillSets]
-admin.site.register(models_registered)
